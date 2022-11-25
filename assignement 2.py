4f5c71d57cfe6e5638ec8373e63e9e374f00b641
elsabellity <118105812+elsabellity@users.noreply.github.com>
2022-11-25 19:29:56 +0100

    Add files via upload

---
+++ assignement 2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My jupyter Notebook on IBM Studio
+
+# **Elsa Bellity**  
+# Data scientist
+
+# *I am interested in data science because I want to work as a data scientist in a financial position*
+
+# ### This code is an addition
+
+# In[6]:
+
+
+a=1+1
+a
+
+
+# -  one
+
+# https://www.google.fr/
+
+# ![image.png](attachment:image.png)
+
+# In[ ]:
+
+
+
+
